8e817f1efef427d70346e7480d1ea2c7c65ff25d
mahendra1290 <mahendra.suthar.1290@gmail.com>
2019-06-10 14:06:41 +0530

    heroku config with static

--- todo_lists/urls.py
+++ todo_lists/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('home/', HomePageView.as_view(), name='home'),
     path('about/', AboutPageView.as_view(), name='about'),
-    path('login/', LoginPageView.as_view(), name='login'),
     path('signup/', SignupPageView.as_view(), name='signup'),
 ]
